7d45575f762dd3d9f12740e97915e5edf57fc71f
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-04-11 10:34:03 -0300

    Create Login.js

---
+++ src/Login.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+//import { Redirect } from 'react-router';
+import './Login.css';
+import { Form, Container, Col, Button } from 'react-bootstrap';
+import axios from 'axios';
+//import Auth from './Auth';
+
+class Login extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			email: '',
+			password: '',
+			redirect: false
+		};
+
+	}
+	
+	onChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
+
+
+	onSubmit(e) {
+		this.setState({
+			[e.target.submit]: e.target.value
+		});
+	}
+
+
+	/*callHome = () => {
+		this.setState({
+			redirect: true
+		});
+	};*/
+
+
+	handleSubmit(e) {
+		e.preventDefault();
+		axios.post('/getToken', {
+		email: this.state.email,
+		password: this.state.password
+		}).then(res => localStorage.setItem('cool-jwt', res.data));
+	}
+
+
+
+
+	render() {
+		/*if (this.state.redirect) {
+			return <Redirect to="/" />;
+		} else {*/
+			return (
+				<div className="lastForm">
+					<div className="formLogin">
+						<Form onSubmit={this.onSubmit.bind(this)}>
+							<h1>Login</h1>
+							<Container>
+								<Form.Row>									
+									<Col>
+									<div className="col-sm-12">
+										<Form.Group as={Col}>
+											<Form.Label htmlFor="email"> E-mail </Form.Label>
+											<Form.Control
+												value={this.state.email}
+												onChange={this.onChange.bind(this)}
+												name="email"
+												id="email"
+												type="email"
+											/>
+										</Form.Group>
+									</div>
+
+									</Col>
+								</Form.Row>						
+									<br />		
+								<Form.Row>							
+									<Col>									
+									<div className="col-sm-12">
+										<Form.Group as={Col}>
+											<Form.Label htmlFor="password"> Senha </Form.Label>
+											<Form.Control
+												value={this.state.password}
+												onChange={this.onChange.bind(this)}
+												name="password"
+												id="password"
+												type="password"
+											/>
+										</Form.Group>
+									</div>
+									</Col>
+								</Form.Row>
+								<Form.Row> 
+									<div className="btnLogin">
+										<Button type="submit" className="" /*onClick={() => this.callHome()}*/>
+											{' '}
+											Cadastrar{' '}
+										</Button>
+									</div>
+								</Form.Row>
+							
+							</Container>
+						</Form>
+					</div>
+				</div>				
+			);
+		}
+	}
+
+
+export default Login;
