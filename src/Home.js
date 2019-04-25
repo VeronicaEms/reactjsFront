72bd88af9f65819c01b744aebcd788d4d7fb6a17
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-04-25 10:04:30 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,67 @@
 import React, { Component } from 'react';
 import './Home.css';
+import { Table, Button } from 'react-bootstrap';
 
 
 class Home extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			releases: [
+				{
+					id: 1,
+					nome: 'primeiro',
+					apelido: 'apelido',
+					email: 'email'
+
+				},
+				{
+					id: 2,
+					nome: 'segundo',
+					apelido: 'apelido',
+					email: 'email'
+
+				},
+				{
+					id: 3,
+					nome: 'terceiro',
+					apelido: 'apelido',
+					email: 'email'
+
+				}
+			]
+		}
+	}
+
+
 	render() {
 		return (
-				<div className="divTexto">											
-						<h1> PEOPLE SOLUTIONS <br/>
-						<b>Inteligência em gestão de negócios</b>
-						</h1>						
-				</div>
-			
+			<Table striped bordered hover variant="dark">
+			<thead>
+			 <tr>
+				<th>Id</th>
+				<th>Nome</th>
+				<th>Apelido</th>
+				<th>Email</th>
+				<th></th>
+
+			 </tr>
+			</thead>
+			<tbody>
+				{this.state.releases.map((release, index) => {
+			 return (
+			<tr key={release.id}>
+			 	<th>{release.id}</th> 
+				<td>{release.nome}</td>
+				<td>{release.apelido}</td>
+				<td>{release.email}</td>
+				<td><Button type="button" className="btn btn-danger">Excluir</Button></td>
+
+			</tr>
+			  )
+			})}
+			</tbody>
+		  </Table>
 		);
 	}
 }
