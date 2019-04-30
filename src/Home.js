b907d293f433d9e2c81fc2271b199a61dd7e6b7a
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-04-30 15:29:22 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,29 @@
 import React, { Component } from 'react';
 import './Home.css';
 import { Table, Button } from 'react-bootstrap';
+import axios from 'axios';
 
 
 class Home extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			releases: [
-				{
-					id: 1,
-					nome: 'primeiro',
-					apelido: 'apelido',
-					email: 'email'
-
-				},
-				{
-					id: 2,
-					nome: 'segundo',
-					apelido: 'apelido',
-					email: 'email'
-
-				},
-				{
-					id: 3,
-					nome: 'terceiro',
-					apelido: 'apelido',
-					email: 'email'
-
-				}
-			]
+			releases : []
+			}
 		}
-	}
 
+		homeUpdate = () =>
+		{
+			this.props.history.push('/update/id_pessoa')
+		}
+
+
+		async componentDidMount(){
+			const releases  = await axios.get('http://localhost:3001/api/employees')
+			console.log(releases);
+			this.setState({releases: releases.data});
+			}   
+	
 
 	render() {
 		return (
@@ -48,22 +39,26 @@ class Home extends Component {
 			 </tr>
 			</thead>
 			<tbody>
-				{this.state.releases.map((release, index) => {
-			 return (
-			<tr key={release.id}>
-			 	<th>{release.id}</th> 
-				<td>{release.nome}</td>
-				<td>{release.apelido}</td>
-				<td>{release.email}</td>
-				<td><Button type="button" className="btn btn-danger">Excluir</Button></td>
+                {this.state.releases.map((release, index) => {
+             return (
+            <tr key={index}>
+                 <th>{release.ID_PESSOA}</th> 
+                <td>{release.NOME}</td>
+                <td>{release.APELIDO}</td>
+                <td>{release.EMAIL}</td>
+                <td><Button type="button" className="btn btn-primary"
+								onClick={() =>this.homeUpdate()}
+								>Editar</Button></td>
+								<td><Button type="button" className="btn btn-danger">Excluir</Button></td>
 
-			</tr>
-			  )
-			})}
-			</tbody>
+            </tr>
+              )
+            })}
+            </tbody>
 		  </Table>
 		);
 	}
 }
 
 export default Home;
+
