e391ce955ecbcd5cad15a614f614010a3f1b3053
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-05-13 14:52:03 -0300

    Create Contact.js

---
+++ src/Contact.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import './Contact.css';
+
+class Contact extends Component {
+	constructor(args) {
+		super(args);
+		this.state = {
+			nome: '',
+			apelido: '',
+			email: '',
+			message: ''
+		};
+	}
+
+	onChange(e) {
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	}
+
+	save(e) {
+		this.setState({
+			message: 'Enviado com sucesso'
+		});
+	}
+
+	render() {
+		return (
+			<div className="card text-white bg-dark mb-3">
+					<div className="card-header"> Contato </div>
+						<div className="card-body">
+								<form>
+								<div className="form-group row">
+									<label for="nome" className="col-sm-3 col-form-label"> Nome </label>
+									<div className="col-sm-8">
+									<input type="text" className="form-control" id="nome" value={this.state.nome}
+									onChange={this.onChange.bind(this)}/>
+									</div>
+								</div>
+
+								<div className="form-group row">
+									<label for="apelido" className="col-sm-3 col-form-label"> Apelido </label>
+									<div className="col-sm-8">
+									<input type="text" className="form-control" id="apelido" value={this.state.apelido}
+									onChange={this.onChange.bind(this)}/>
+									</div>
+								</div>
+
+								<div className="form-group row">
+									<label for="email" className="col-sm-3 col-form-label"> Email </label>
+									<div className="col-sm-8">
+									<input type="email" className="form-control" id="email" value={this.state.email}
+									onChange={this.onChange.bind(this)}/>
+									</div>
+								</div>
+
+								<div className="input-group">
+								<div className="input-group-prepend">
+									<span className="input-group-text"></span>
+								</div>
+								<textarea className="form-control" aria-label="Com textarea"
+								 id="message" placeholder="Escreva sua mensagem aqui"
+								 value={this.state.message} onChange={this.onChange.bind(this)}>
+								 </textarea>
+								</div>
+
+
+						<div className="form-group row">
+    								<div>
+      									<button type="submit" className="btn btn-primary" onClick={this.save.bind(this)}>
+										  {' '}
+										Enviar{' '}
+										</button>
+    								</div>
+ 								</div>
+						
+						</form>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Contact;
