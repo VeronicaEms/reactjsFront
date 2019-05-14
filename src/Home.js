8678c5ec68231ef245036c475e860a0239e5c99d
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-05-14 11:47:07 -0300

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.css";
 import './Home.css';
 
 
@@ -18,12 +19,12 @@ class Home extends Component {
           })}
 
 
-        homeUpdate = (id_pessoa) =>
+         homeUpdate = (id_pessoa) =>
         {
             this.props.history.push(`/update/${id_pessoa}`)
-        }
+        } 
 
-        homeRemove = async id_pessoa => {
+         homeRemove = async id_pessoa => {
           console.log(">>> HOMEREMOVE()", id_pessoa);
           await axios
             .delete(`http://localhost:3001/api/employees/${id_pessoa}`)
@@ -33,7 +34,7 @@ class Home extends Component {
               console.log(">USUÁRIO REMOVIDO", res.data.affectedRows);
               }
             });
-        };
+        }; 
 
         getAllData = async () => {
           const res = await axios.get("http://localhost:3001/api/employees");
@@ -56,41 +57,47 @@ class Home extends Component {
 
     render() {
         return (
+        <form className="form-inline">
+          <input className="form-control" type="search" id="search" value={this.state.search}
+            onChange={ e => this.onChange(e) } placeholder="Search" aria-label="Search"/>
+              <button className="btn btn-primary my-2 my-sm-0" type="button" onClick={this.searchEmail}> Search </button>
             <table className="table table-hover table-dark">
             <thead>
-             <tr>
+             <tr>                
                 <th scope="col">Id</th>
                 <th scope="col">Nome</th>
                 <th scope="col">Apelido</th>
                 <th scope="col">Email</th>
-                <th scope="col">editar </th>
-                <th scope="col">excluir  </th>
+                <th scope="col"></th>
+                <th scope="col"></th>
              </tr>
             </thead>
             <tbody>
                 {this.state.listEmployee.map((release, index) => {
             return (
             <tr key={index}>
-                 <td className="text-center" scope="row">{release.ID_PESSOA}</td> 
+                 <th className="text-center" scope="row">{release.ID_PESSOA}</th> 
                 <td>{release.NOME}</td>
                 <td>{release.APELIDO}</td>
                 <td>{release.EMAIL}</td>
-                <td><button className="btn btn-primary" type="button"
-                onClick={() =>this.homeUpdate(release.ID_PESSOA)}
+                <td><button className="btn btn-primary ml-0 mt-0" type="button"
+                 onClick={() =>this.homeUpdate(release.ID_PESSOA)} 
                 >Editar</button></td>
-                <td><button className="btn btn-danger" type="button"
-                onClick={() => this.homeRemove(release.ID_PESSOA)}
+                <td><button className="btn btn-danger ml-0 mt-0" type="button"
+                onClick={() => this.homeRemove(release.ID_PESSOA)} 
                 >Excluir</button></td>
 						</tr>
             );
             })}
             </tbody>
           </table>
+        </form>
         );
 		}
 }
 
 export default Home;
+
 
 
 
