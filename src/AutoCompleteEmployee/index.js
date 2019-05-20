626bcd15ab4bf7dc386a2b44c70b7cf5599581df
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-05-20 15:22:23 -0300

    Update index.js

--- src/AutoCompleteEmployee/index.js
+++ src/AutoCompleteEmployee/index.js
@@ -13,14 +13,14 @@ export default class AutoCompleteEmployee extends Component {
 
     componentDidMount() {
         const { listEmployee } = this.props; // Aqui estou pegando a lista que veio através da prop listEmployee lá do Home.js
-      //  console.log(this.props, listEmployee);
         const list = listEmployee.map(element => {
             return element.NOME;
         }); //Essa é a lista já filtrada em um único elemento.
 
         //Aqui alterei o estado do nosso componente com as sugestões
 
-        this.setState(state => ({...state, autocompleteEmployee: list})); 
+        //this.setState(state => ({...state, autocompleteEmployee: list})); 
+        this.setState({autocompleteEmployee: list});
     }
 
     render() {
